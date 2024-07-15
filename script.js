6809c215a04a3a0ca2d09bf459c1eb4a3e099546
Thulasimani <157271044+thulasimani003@users.noreply.github.com>
2024-07-15 16:53:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function findLongestWord(str){
+    const words=str.split(' ');
+    let longestWord='';
+    for(let word of words){
+        if(word.length>longestWord.length){
+            longestWord=word;
+        }
+    }
+    return longestWord;
+}
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
